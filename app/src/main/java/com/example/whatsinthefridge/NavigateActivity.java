312b6fba4e16de0de96926ec7aaf054fb6ff3529
almasteinman <alma.steinman@gmail.com>
2024-11-26 10:57:42 +0200

    create navigation page

--- app/src/main/java/com/example/whatsinthefridge/NavigateActivity.java
+++ app/src/main/java/com/example/whatsinthefridge/NavigateActivity.java
@@ -1,9 +1,6 @@
 package com.example.whatsinthefridge;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class NavigateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigate);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // Find the button by its ID
-        Button navigateButton = findViewById(R.id.navigateButton);
-
-        // Set click listener for the button
-        navigateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to start the NavigateActivity
-                Intent intent = new Intent(MainActivity.this, NavigateActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
