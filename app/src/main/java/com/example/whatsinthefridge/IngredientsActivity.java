11eb3776d84240605ba8bebc25ac2750b546538c
almasteinman <alma.steinman@gmail.com>
2024-12-23 13:21:20 +0200

    create ingredients page and recipeList page connect to main

--- app/src/main/java/com/example/whatsinthefridge/IngredientsActivity.java
+++ app/src/main/java/com/example/whatsinthefridge/IngredientsActivity.java
@@ -1,6 +1,9 @@
 package com.example.whatsinthefridge;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class NavigateActivity extends AppCompatActivity {
+public class IngredientsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_navigate);
+        setContentView(R.layout.activity_ingredients);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Find the button by its ID
+        Button recipeButton = findViewById(R.id.recipeButton);
+
+        // Set click listener for the button
+        recipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the RecipeListActivity
+                Intent intent = new Intent(IngredientsActivity.this, RecipeListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
