08d9e22ca86bbb0473d795f0f440fa2425436dfe
almasteinman <alma.steinman@gmail.com>
2025-01-21 10:32:34 +0200

    RecipeAdapter and RecyclerView

--- app/src/main/java/com/example/whatsinthefridge/RecipeListActivity.java
+++ app/src/main/java/com/example/whatsinthefridge/RecipeListActivity.java
@@ -7,6 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.whatsinthefridge.Recipe;
+import com.google.android.material.carousel.CarouselLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipeListActivity extends AppCompatActivity {
 
@@ -20,6 +27,25 @@ public class RecipeListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
+
+    // Find RecyclerView
+    RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+    // Set LayoutManager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        // Create data
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe("Pasta", "Delicious pasta with tomato sauce",R.drawable.pasta ));
+        recipes.add(new Recipe("Pizza", "Cheesy and tasty pizza", R.drawable.pizza));
+        recipes.add(new Recipe("Salad", "Healthy green salad", R.drawable.salad));
+
+    // Set Adapter
+    RecipeAdapter adapter = new RecipeAdapter(recipes);
+    recyclerView.setAdapter(adapter);
 }
+}
+
